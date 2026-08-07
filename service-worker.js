550da74e64cff6b7362687abdb2ae13a3dc1d4a6
zezo20234 <zezo_z4ever@outlook.com>
2026-08-07 17:33:07 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,37 +1,68 @@
-const CACHE_NAME = 'ultimate-team-v1';
-const urlsToCache = [
-  './',
-  './index.html',
-  './styles.css',
-  './app.js',
-  './database.js',
-  './match.js',
-  './matchmaking.js',
-  './players.js',
-  './playerDatabase.js',
-  './market.js',
-  './packs.js',
-  './seedingService.js',
-  './auth.js',
-  './firebase.js',
-  './utils.js'
+const CACHE_NAME = "money-manager-v1";
+
+const FILES_TO_CACHE = [
+    "./",
+    "./index.html",
+    "./manifest.json"
 ];
 
-self.addEventListener('install', event => {
-  event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(cache => cache.addAll(urlsToCache))
-  );
+
+self.addEventListener("install", (event) => {
+
+    event.waitUntil(
+
+        caches.open(CACHE_NAME)
+        .then((cache)=>{
+
+            return cache.addAll(FILES_TO_CACHE);
+
+        })
+
+    );
+
 });
 
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => {
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      })
-  );
+
+
+self.addEventListener("activate", (event)=>{
+
+    event.waitUntil(
+
+        caches.keys().then((keys)=>{
+
+            return Promise.all(
+
+                keys.map((key)=>{
+
+                    if(key !== CACHE_NAME){
+
+                        return caches.delete(key);
+
+                    }
+
+                })
+
+            );
+
+        })
+
+    );
+
+});
+
+
+
+self.addEventListener("fetch",(event)=>{
+
+    event.respondWith(
+
+        caches.match(event.request)
+        .then((response)=>{
+
+            return response || fetch(event.request);
+
+        })
+
+    );
+
 });
